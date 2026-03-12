a79874e2d025c8d8671a0a5d1bfd9bbaa6c2d3df
Kshitij Jain <2024btechaimlkshitij18489@poornima.edu.in>
2026-03-12 22:14:47 +0530

    Update the landing.hsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AI from './pages/AI';
+import Landing from './pages/Landing';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<AI />} />
+        {/* This sets Landing as the default home page */}
+        <Route path="/" element={<Landing />} />
+        
+        {/* Other routes */}
         <Route path="/ai" element={<AI />} />
       </Routes>
     </Router>
